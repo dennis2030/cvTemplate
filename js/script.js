4faac2320fa4bc29cf1c00620e50e1a126fe918b
danielhsieh <danielhsieh@appletekiMacBook-Pro-2.local>
2014-08-10 21:07:14 +0800

    first commit.

---
+++ js/script.js
@@ -0,0 +1,12 @@
+"use strict";
+$(document).ready(function(){
+	$("#canvas-button").click(function() {
+		$("#navigator").toggleClass('expand');
+		return false;
+	});
+	$('ul#navigator li a').click(function() {
+		$('body, html').stop();
+		$('body, html').animate({ scrollTop: ($($(this).attr("href")).offset().top) }, "slow");
+		return false;
+	});
+});
