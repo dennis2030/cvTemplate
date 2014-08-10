abf28bc51bc6e2fa95298fd72e7603a5b85d7c0a
Bingdow <bingdow@gmail.com>
2014-08-10 21:00:06 +0800

    fake version.

--- js/script.js
+++ js/script.js
@@ -2,5 +2,11 @@
 $(document).ready(function(){
 	$("#canvas-button").click(function() {
 		$("#navigator").toggleClass('expand');
+		return false;
+	});
+	$('ul#navigator li a').click(function() {
+		$('body, html').stop();
+		$('body, html').animate({ scrollTop: ($($(this).attr("href")).offset().top) }, "slow");
+		return false;
 	});
 });
